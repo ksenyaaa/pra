f44d5c7083d803a525e9e30245fcf032bcf2f548
ksenyaaa <kseniasidneva3@gmail.com>
2024-07-02 17:06:14 +0300

    HW1

--- app/src/main/java/com/example/practice2/MainActivity.kt
+++ app/src/main/java/com/example/practice2/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.practice2
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.practice2.databinding.ActivityFragmentBinding
+import com.example.practice2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+    private var controller: NavController? = null
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
         }
+
+        controller = (supportFragmentManager.findFragmentById(R.id.container) as
+                NavHostFragment).navController
+
     }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        controller?.navigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
