1eece3c8f88745f80f9efe181b3e9025ff2fc59f
ksenyaaa <kseniasidneva3@gmail.com>
2024-07-04 16:50:42 +0300

    HW1

--- app/src/main/java/com/example/practice2/FilmHolder.kt
+++ app/src/main/java/com/example/practice2/FilmHolder.kt
@@ -3,11 +3,15 @@ package com.example.practice2
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.practice2.databinding.ItemFilmBinding
 
-class FilmHolder(private val bilding: ItemFilmBinding): ViewHolder(bilding.root) {
+class FilmHolder(private val bilding: ItemFilmBinding, private val glide: RequestManager): ViewHolder(bilding.root) {
     fun onBind(film: Film){
         bilding.run {
             tvFilm.text = film.name
             tvTypes.text = film.types
+
+            Glide.with(itemView.context)
+                .load(film.url)
+                .into(ivImage)
         }
     }
 }
